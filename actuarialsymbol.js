d3c6e32730ca66f4ce9aa21238669141d4e6937b
Marc-André Devost <marc-andre.devost.1@ulaval.ca>
2021-04-07 23:05:34 -0400

    update to JS file with latest changes to macro

--- actuarialsymbol.js
+++ actuarialsymbol.js
@@ -2,7 +2,7 @@
  *
  *  actuarialsymbol.js
  *  
- *  Implements the actuarialsymbol and actuarialangle Packages for LaTeX input.
+ *  Implements part of the actuarialsymbol and actuarialangle Packages for LaTeX input.
  *  
  *  ---------------------------------------------------------------------
  *  
@@ -23,16 +23,108 @@
  */MathJax.Hub.Register.StartupHook("TeX Jax Ready", function() {
     MathJax.InputJax.TeX.Definitions.Add({
         macros: {
-            quantity: [ "Macro", "{\\left\\{ #1 \\right\\}}", 1 ],
-            endowxn: [ "Macro", "{\\require{enclose} {x:\\enclose{actuarial}{n}}}"],
-            angln: [ "Macro", "{\\require{enclose} \\enclose{actuarial}{n}}"],
-            Ax: [ "Macro", "{A_{#1}}", 1 ],
-            bAx: [ "Macro", "{\\bar{A}_{#1}}", 1 ],
-            dAx: [ "Macro", "{{\\ddot A}_{#1}}", 1 ],
-            ax: [ "Macro", "{a_{#1}}", 1 ],
-            bax: [ "Macro", "{\\bar{a}_{#1}}", 1 ],
-            dax: [ "Macro", "{{\\ddot a}_{#1}}", 1 ],
-            actsymb : [ "Macro", "{_{#1}^{#2}{#3}_{#4}^{#5}}", 5],
+
+	    endow: ["Macro","{\\require{enclose} {{#1}:\\enclose{actuarial}{#2}}}", 2],
+            endowxn: ["Macro","{\\require{enclose} {x:\\enclose{actuarial}{n}}}"],
+            
+            pureendow: ["Macro","{\\require{enclose} {{#1}:\\overset{1}{\\enclose{actuarial}{#2}}}}", 2],
+            pureendowxn: ["Macro","{\\require{enclose} {x:\\overset{1}{\\enclose{actuarial}{n}}}}"],
+            
+            term : ["Macro","{\\require{enclose} {\\overset{1}{#1}:\\enclose{actuarial}{#2}}}", 2],
+            termxn : ["Macro","{\\require{enclose} {\\overset{1}{x}:\\enclose{actuarial}{n}}}"],
+            
+            angl: ["Macro","{\\require{enclose} \\enclose{actuarial}{#1}}", 1],
+            angln: ["Macro","{\\require{enclose} \\enclose{actuarial}{n}}"],
+            
+            nthtop : ["Macro","\\overset{#1}{#2}", 2],
+            
+            joint : ["Macro","\\overline{#1}", 1],
+
+            itop : ["Macro","\\overset{1}{#1}", 1],
+            iitop : ["Macro","\\overset{2}{#1}", 1],
+            iiitop : ["Macro","\\overset{3}{#1}", 1],
+            
+            ibottom : ["Macro","\\underset{1}{#1}", 1],
+            iibottom : ["Macro","\\underset{2}{#1}", 1],
+            iiibottom : ["Macro","\\underset{3}{#1}", 1],
+            
+            
+            Ax: ["Macro", "{A^{#1}_{#2}}", 2, ""],
+            bAx: ["Macro", "{\\bar{A}^{#1}_{#2}}", 2, ""],
+            dAx: ["Macro", "{{\\ddot A}^{#1}_{#2}}", 2, ""],
+            tAx: ["Macro", "{{\\tilde A}^{#1}_{#2}}", 2, ""],
+            
+            ax: ["Macro", "{a^{#1}_{#2}}", 2, ""],
+            bax: ["Macro", "{\\bar{a}^{#1}_{#2}}", 2, ""],
+            dax: ["Macro", "{\\ddot{a}^{#1}_{#2}}", 2, ""],
+            tax: ["Macro", "{\\tilde{a}^{#1}_{#2}}", 2, ""],
+            
+            Aringx: ["Macro", "{\\mathring{A}^{#1}_{#2}}", 2, ""],
+            aringx: ["Macro", "{\\mathring{a}^{#1}_{#2}}", 2, ""],
+            
+            Eringx: ["Macro", "{\\mathring{E}^{#1}_{#2}}", 2, ""],
+            eringx: ["Macro", "{\\mathring{e}^{#1}_{#2}}", 2, ""],
+            
+            Sringx: ["Macro", "{\\mathring{S}^{#1}_{#2}}", 2, ""],
+            sringx: ["Macro", "{\\mathring{s}^{#1}_{#2}}", 2, ""],
+            
+            dx : ["Macro", "{d^{#1}_{#2}}", 2, ""],
+            Dx : ["Macro","{\\mathscr{D}^{#1}_{#2}}", 2, ""],
+            
+            IA : ["Macro", "(IA)"],
+            IbA : ["Macro", "(I\\bar A)"],
+            bIbA : ["Macro", "(\\bar I\\bar A)"],
+            ImA : ["Macro", "(I^{(m)}A)"],
+            ImbA : ["Macro", "(I^{(m)}\\bar A)"],
+            bImbA : ["Macro", "(\\bar I^{(m)}\\bar A)"],
+            Is : ["Macro", "(Is)"],
+            Ibs : ["Macro", "(I \\bar s)"],
+            Ids : ["Macro", "(I \\ddot s)"],
+            
+            Ex : ["Macro","{E^{#1}_{#2}}", 2, ""],
+            
+            DA : ["Macro", "(DA)"],
+            DbA : ["Macro", "(D\\bar A)"],
+            bDbA : ["Macro", "(\\bar D\\bar A)"],
+            DmA : ["Macro", "(D^{(m)}A)"],
+            DmbA : ["Macro", "(D^{(m)}\\bar A)"],
+            bDmbA : ["Macro", "(\\bar D^{(m)}\\bar A)"],
+            Ds : ["Macro", "(Ds)"],
+            Dbs : ["Macro", "(D \\bar s)"],
+            Dds : ["Macro", "(D \\ddot s)"],
+            
+            lx : ["Macro", "{\\ell^{#1}_{#2}}", 2, ""],
+            Lx : ["Macro","{\\mathscr{L}^{#1}_{#2}}", 2, ""],
+            
+            px : ["Macro", "{p^{#1}_{#2}}", 2, ""],
+            Px : ["Macro", "{P^{#1}_{#2}}", 2, ""],
+            
+            premium : ["Macro", "{P^{#1}\ \\left({#2}\\right)}", 2, ""],
+            bpremium : ["Macro", "{\\bar{P}^{#1}\\left({#2}\\right)}", 2, ""],
+            tpremium : ["Macro", "{\\tilde{P}^{#1}\\left({#2}\\right)}", 2, ""],
+
+            paidup : ["Macro", "{W^{#1}\ \\left({#2}\\right)}", 2, ""],
+            bpaidup : ["Macro", "{\\bar{W}^{#1}\\left({#2}\\right)}", 2, ""],
+            tpaidup : ["Macro", "{\\tilde{W}^{#1}\\left({#2}\\right)}", 2, ""],
+
+
+            reserve : ["Macro", "{V^{#1}\ \\left({#2}\\right)}", 2, ""],
+            breserve : ["Macro", "{\\bar{V}^{#1}\\left({#2}\\right)}", 2, ""],
+            treserve : ["Macro", "{\\tilde{V}^{#1}\\left({#2}\\right)}", 2, ""],
+
+            qx : ["Macro", "{q^{#1}_{#2}}", 2, ""],
+            Qx : ["Macro", "{Q^{#1}_{#2}}", 2, ""],
+
+            sx: ["Macro", "{s^{#1}_{#2}}", 2, ""],
+            bsx: ["Macro", "{\\bar{s}^{#1}_{#2}}", 2, ""],
+            dsx: ["Macro", "{\\ddot{s}^{#1}_{#2}}", 2, ""],
+
+            Vx : ["Macro", "{V^{#1}_{#2}}", 2, ""],
+            Wx : ["Macro", "{W^{#1}_{#2}}", 2, ""],
+
+            actsymb : ["Macro","{_{#1}^{#2}{#3}_{#4}^{#5}}", 5]
+
+
         }
     });
 });
